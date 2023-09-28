77cc264feed9f4ab05a29604204eba0f471a43c9
TestJBr <145455827+TestJBr@users.noreply.github.com>
2023-09-28 11:55:01 +0200

    Update centos-lamp.sh

--- scripts/centos-lamp.sh
+++ scripts/centos-lamp.sh
@@ -29,7 +29,7 @@ mysql -u root -e "SHOW DATABASES";
 
 #Download Starter Content
 cd /vagrant
-sudo -u vagrant wget -q https://github.com/TestJBr/vagrant/blob/master/files/index.html
+sudo -u vagrant wget -q https://raw.githubusercontent.com/TestJBr/vagrant/master/files/index.html
 sudo -u vagrant wget -q https://github.com/TestJBr/vagrant/blob/master/files/info.php
 
 service httpd restart
